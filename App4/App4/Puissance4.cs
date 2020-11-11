3018df86f34a30869a200504ac3d7f34a07b8099
enicayzer <seataddicted@gmail.com>
2020-11-11 14:52:52 +0100

    Retour arrière

--- App4/App4/Puissance4.cs
+++ App4/App4/Puissance4.cs
@@ -13,6 +13,7 @@ namespace App4
         public int?[,] matrice; // [x,y] : x => ligne et y => colonne
         public Joueur joueur { get; set; }
         public bool isJoueur1 { get; set; }
+        public bool isJoueurAlphaBeta { get; set; }
         #endregion
 
         #region Constructeur
@@ -232,7 +233,8 @@ namespace App4
         /// Méthode qui retourne la colonne choisie par l'IA
         /// </summary>
         /// <returns></returns>
-        public int DecisionIA(bool isJoueur1)
+        /// <returns></returns>
+        public int DecisionIA(bool isJoueur1, int profondeur, bool isJoueurAlphaBeta)
         {
             // Récupération des valeurs en retour de l'IA 
             if (isJoueur1)
@@ -243,8 +245,9 @@ namespace App4
             {
                 this.isJoueur1 = false;
             }
+            this.isJoueurAlphaBeta = isJoueurAlphaBeta;
             List<int?> retourIA;
-            retourIA = Max(this, 4);
+            retourIA = Max(this, profondeur);
             return (int)retourIA[0].Value;
         }
 
@@ -290,7 +293,7 @@ namespace App4
                         maximumPoints[1] = prochainCoup[1];
                         a = prochainCoup[1];
                     }
-                    if(a >= b && jeu.EstAlphaBeta)
+                    if(a >= b && isJoueurAlphaBeta)
                     {
                         return maximumPoints;
                     }
@@ -330,7 +333,7 @@ namespace App4
                         minimumPoints[1] = prochainCoup[1];
                         b = prochainCoup[1];
                     }
-                    if(a >= b && jeu.EstAlphaBeta)
+                    if(a >= b && isJoueurAlphaBeta)
                     {
                         return minimumPoints;
                     }
